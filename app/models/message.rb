b9341c1ca50c461938b94c8a6f0426dfec05561a
SHINJII WATANABE <ancientmask@gmail.com>
2016-08-29 12:07:20 +0000

    set validation for :age

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  # 年齢は必須入力かつ0以上の3桁までの数字
+  validates :age , numericality: {
+    only_integer: true, greater_than: 0, less_than:1000
+    } , presence: true
 end
